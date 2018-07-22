d7b10b017759570b55b0d38a4e14113404e3b178
gnehs <jayuiop@gmail.com>
2018-07-22 19:54:57 +0800

    後端快完工ㄌ

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const fs = require('fs'); //檔案系統
-var programData = { "DSM": { "cookie": "" } }
 const config = require('./config.json'); // 設定檔
-const syno = require('./modules/DSM.js'); // 請求小夥伴
 const schedule = require('node-schedule'); // 很會計時ㄉ朋友
-const express = require('express'); // Node.js Web 架構
+//express
+const express = require('express');
 const session = require('express-session');
 const helmet = require('helmet'); // 防範您的應用程式出現已知的 Web 漏洞
 const bodyParser = require('body-parser'); // 讀入 post 請求
-const app = express()
+const app = express(); // Node.js Web 架構
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +20,10 @@ app.use(session({
 const moment = require('moment-timezone');
 moment.locale('zh-tw');
 moment.tz.setDefault("Asia/Taipei");
+//請求
+var request = require('request');
+var j = request.jar()
+var request = request.defaults({ jar: j })
 
 // 設定 js icon css 目錄
 app.use('/js', express.static('js'))
@@ -34,33 +37,106 @@ app.listen(3000, async() => {
     console.log(moment().format("🕒 YYYY/MM/DD HH:mm"))
     console.log("////////////////////////")
 
-    var a = await syno.login(config.DSM)
+    var a = await login(config.DSM)
     if (!a.success) {
         console.error("登入失敗，請檢查您的設定檔是否正確")
         process.exit()
     } else {
-        // 拿到ㄌ騙吃騙喝的餅乾ㄌ
-        programData.DSM.cookie = a.cookie[0]
+        //var b = await syno.random100(config.DSM)
+        //console.log(b)
     }
 })
 
 // 首頁
 app.get('/', (req, res) => {
-    console.log(programData)
-        // 沒登入的快去啦
+    // 沒登入的快去啦
     if (req.session.pass != config.PokaPlayer.password && config.PokaPlayer.passwordSwitch)
         res.redirect("/login/")
     else
         res.render('index') //有登入給首頁吼吼
 })
 
-// 登入
-app.get('/', (req, res) => {
-    res.render('login')
+// Reverse Proxy
+app.get('/nas/:url', (req, res) => {
+    if (req.session.pass != config.PokaPlayer.password && config.PokaPlayer.passwordSwitch)
+        res.send('請登入')
+    else {
+        var url = `${config.DSM.protocol}://${config.DSM.host}:${config.DSM.port}/${decodeURIComponent(req.params.url)}`
+        request.get(url).pipe(res);
+    }
 })
+
+// api
+app.get('/api/:apireq', async(req, res) => {
+    var apireq = JSON.parse(req.params.apireq)
+        /*
+        should be like this
+        {
+            "CGI_PATH":"",
+            "API_NAME","",
+            "METHOD":"",
+            "PARAMS":"",
+            "VERSION":2,
+            "PARAMS":{
+                "aaa":"bbb",
+                "ccc":"ddd"
+            }
+        }
+        */
+    if (req.session.pass == config.PokaPlayer.password && !config.PokaPlayer.passwordSwitch)
+        res.send(await syno.api(config.DSM, apireq.CGI_PATH, apireq.API_NAME, apireq.METHOD, apireq.VERSION, apireq.PARAMS))
+    else
+        res.send('請登入')
+})
+
+// 登入
+app.get('/login/', (req, res) => {
+    if (req.session.pass == config.PokaPlayer.password && !config.PokaPlayer.passwordSwitch)
+        res.redirect("/")
+    else
+        res.render('login')
+})
+app.post('/login/', (req, res) => {
+    req.session.pass = req.body['userPASS']
+    if (req.body['userPASS'] != config.PokaPlayer.password && config.PokaPlayer.passwordSwitch)
+        res.render('login')
+    else
+        res.redirect("/")
+});
+// 登出
+app.get('/logout/', (req, res) => {
+    req.session.destroy()
+    res.redirect("/")
+});
 var updateCookie = schedule.scheduleJob("'* */12 * * *'", async function() {
     //請求登入 Cookie
     console.log("正在自動更新令牌")
     var a = await syno.login(config.DSM)
-    programData.DSM.cookie = a.cookie
 });
+
+
+
+async function login(dsm) {
+    return new Promise(function(resolve, reject) {
+        var url = `${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=1&account=${dsm.account}&passwd=${dsm.password}&session=AudioStation&format=cookie`
+        request(url, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
+        });
+    });
+}
+
+async function api(dsm, CGI_PATH, API_NAME, METHOD, VERSION = 1, PARAMS) {
+    return new Promise(function(resolve, reject) {
+        request(`${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/${CGI_PATH}?api=${API_NAME}&method=${METHOD}&version=${VERSION}${PARAMS}`, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
+        });
+    });
+}
--- modules/DSM.js
+++ modules/DSM.js
@@ -1,25 +1,48 @@
-const request = require('request');
-
+var request = require('request');
+var j = request.jar()
+var request = request.defaults({ jar: j })
 exports.login = async(dsm) => {
-        return new Promise(function(resolve, reject) {
-            var url = `${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=1&account=${dsm.account}&passwd=${dsm.password}&session=AudioStation&format=cookie`
-            request(url, function(error, res, body) {
-                if (!error && res.statusCode == 200) {
-                    resolve({ "cookie": res.headers["set-cookie"], "success": JSON.parse(body).success });
-                } else {
-                    resolve(error);
-                }
-            });
+    return new Promise(function(resolve, reject) {
+        var url = `${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=1&account=${dsm.account}&passwd=${dsm.password}&session=AudioStation&format=cookie`
+        request(url, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
         });
-    }
-    //https://nas.gnehs.net/webapi/AudioStation/song.cgi
-    /*
-    additional	song_tag,song_audio,song_rating
-    api	SYNO.AudioStation.Song
-    library	shared
-    limit	100
-    method	list
-    sort_by	random
-    version	3
-    */
-    //https://nas.gnehs.net/webapi/AudioStation/stream.cgi/0.mp3?sid=24oFv8yyNrnVw1820PEN591901&api=SYNO.AudioStation.Stream&version=2&method=stream&id=music_1678&_dc=1532249856580
+    });
+}
+exports.proxy = async(url) => {
+    return new Promise(function(resolve, reject) {
+        request(url, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
+        });
+    });
+}
+exports.api = async(dsm, CGI_PATH, API_NAME, METHOD, VERSION = 1, PARAMS) => {
+    return new Promise(function(resolve, reject) {
+        request(`${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/${CGI_PATH}?api=${API_NAME}&method=${METHOD}&version=${VERSION}${PARAMS}`, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
+        });
+    });
+}
+exports.random100 = async(dsm) => {
+    return new Promise(function(resolve, reject) {
+        request(`${dsm.protocol}://${dsm.host}:${dsm.port}/webapi/AudioStation/song.cgi?api=SYNO.AudioStation.Song&method=list&additional=song_tag,song_audio,song_rating&library=shared&limit=100&sort_by=random&version=1`, function(error, res, body) {
+            if (!error && res.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(error);
+            }
+        });
+    });
+}
