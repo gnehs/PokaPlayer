e3be7e44142f9e4bcfda6be64464050629a9c841
gnehs <jayuiop@gmail.com>
2018-07-24 22:09:19 +0800

    snackbar 調整 timeout

--- js/login.js
+++ js/login.js
@@ -4,12 +4,13 @@ $(function() {
     if (userPASS) {
         mdui.snackbar({
             message: '偵測到已儲存的密碼，正在嘗試登入',
+            timeout: 1000
         });
         $.post("/login/", { userPASS: userPASS }, function(data) {
             if (data == 'success') {
-                mdui.snackbar({ message: '登入成功' });
+                mdui.snackbar({ message: '登入成功', timeout: 1000 });
                 document.location.href = "/";
-            } else mdui.snackbar({ message: '登入失敗' });
+            } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
         });
     }
 });
@@ -18,10 +19,10 @@ function check() {
 
     $.post("/login/", { userPASS: $("#userPASS").val() }, function(data) {
         if (data == 'success') {
-            mdui.snackbar({ message: '登入成功' });
+            mdui.snackbar({ message: '登入成功', timeout: 1000 });
             window.localStorage["userPASS"] = $("#userPASS").val()
             document.location.href = "/";
-        } else mdui.snackbar({ message: '登入失敗' });
+        } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
     });
     return false
 }
