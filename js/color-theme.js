89cae9f2934aea930885b44cec5faab326d6ce0e
gnehs <jayuiop@gmail.com>
2018-08-03 23:50:20 +0800

    主題功能修改，預設不套用主題（由 js 套用）

--- js/color-theme.js
+++ js/color-theme.js
@@ -1,32 +1,17 @@
 $(function() {
     // 檢查有沒有設定顏色
-    if (window.localStorage["mdui-theme-primary"]) {
-        var classStr = $('body').attr('class');
-        var classs = classStr.split(' ');
-        for (i = 0, len = classs.length; i < len; i++) {
-            if (classs[i].indexOf('mdui-theme-primary-') === 0) {
-                $('body').removeClass(classs[i])
-            }
-        }
-        $('body').addClass(`mdui-theme-primary-${window.localStorage["mdui-theme-primary"]}`)
-            //設定顏色
-        var metaThemeColor = document.querySelector("meta[name=theme-color]");
-        metaThemeColor.setAttribute("content", $('header>div:first-child').css("background-color"));
-    } else {
-        window.localStorage["mdui-theme-primary"] = indigo
-    }
-    if (window.localStorage["mdui-theme-accent"]) {
-        var classStr = $('body').attr('class');
-        var classs = classStr.split(' ');
-        for (i = 0, len = classs.length; i < len; i++) {
-            if (classs[i].indexOf('mdui-theme-accent-') === 0) {
-                $('body').removeClass(classs[i])
-            }
-        }
-        $('body').addClass(`mdui-theme-accent-${window.localStorage["mdui-theme-accent"]}`)
-    } else window.localStorage["mdui-theme-accent"] = pink
-    if (window.localStorage["mdui-theme-color"] == "true") {
-        $('body').addClass("mdui-theme-layout-dark")
-    }
+    if (!window.localStorage["mdui-theme-primary"])
+        window.localStorage["mdui-theme-primary"] = "indigo"
+    if (!window.localStorage["mdui-theme-accent"])
+        window.localStorage["mdui-theme-accent"] = "pink"
 
+    // 顏色設定
+    $('body').addClass(`mdui-theme-primary-${window.localStorage["mdui-theme-primary"]}`)
+    $('body').addClass(`mdui-theme-accent-${window.localStorage["mdui-theme-accent"]}`)
+    if (window.localStorage["mdui-theme-color"] == "true")
+        $('body').addClass("mdui-theme-layout-dark")
+
+    // 設定狀態欄顏色
+    var metaThemeColor = document.querySelector("meta[name=theme-color]");
+    metaThemeColor.setAttribute("content", $('header>div:first-child').css("background-color"));
 });
