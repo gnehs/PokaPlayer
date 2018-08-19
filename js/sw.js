de4c09832264dcd5c882dfe37a69577bde34ca01
gnehs <jayuiop@gmail.com>
2018-08-19 14:33:02 +0800

    修正 GitHub API 被快取導致怎麼檢查都沒更新

--- js/sw.js
+++ js/sw.js
@@ -34,7 +34,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', function(event) {
     //console.log(event.request)
-    if (event.request.destination != "audio" && event.request.method == "GET" && !event.request.url.match(/socket.io|meting|info|ping/))
+    if (event.request.destination != "audio" && event.request.method == "GET" && !event.request.url.match(/socket.io|meting|info|ping|github/))
         event.respondWith(
             caches.match(event.request).then(function(response) {
                 // 擷取 HTTP 請求
