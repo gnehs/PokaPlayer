556965c4871ec197a4ffa46efe313522b1bb5f76
gnehs <jayuiop@gmail.com>
2018-08-25 14:25:48 +0800

    Medium

--- public/js/setting.js
+++ public/js/setting.js
@@ -66,7 +66,7 @@ async function showSettings() {
             ${settingsItem("Low","低音質，128K，夭壽靠北，在網路夭壽慢的情況下請選擇此選項","","",
                             `onclick="window.localStorage['musicRes']='Low'" mdui-dialog-close`)}
             ${settingsItem("Medium","中等音質，音質較高音質差，可在 4G 網路下流暢的串流","","",
-                            `onclick="window.localStorage['musicRes']='High'" mdui-dialog-close`)}
+                            `onclick="window.localStorage['musicRes']='Medium'" mdui-dialog-close`)}
             ${settingsItem("High","高音質，音質較原始音質略差，可在 4G 網路下流暢的串流","","",
                             `onclick="window.localStorage['musicRes']='High'" mdui-dialog-close`)}
             ${settingsItem("Original","原始音質，在網路狀況許可下，建議選擇此選項聆聽高音質音樂","","",
