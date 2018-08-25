2f437e13d54f36cb7a5a45ef9e2bd3c505336e7a
gnehs <jayuiop@gmail.com>
2018-08-25 22:51:15 +0800

    移除選擇歌詞來源之選項

--- public/js/api.js
+++ public/js/api.js
@@ -24,25 +24,3 @@ async function getLrc(artist, title, id = false, source) {
 async function searchLrc(keyword) {
     return await axios.get(`/pokaapi/searchLyrics/?keyword=${encodeURIComponent(keyword)}`)
 }
-
-// 備用
-async function getMetingSearchResult(keyword, limit = 1) {
-    let meting = window.localStorage["lrcMetingUrl"],
-        server = 'netease',
-        search = await axios.get(`${meting}?server=${server}&type=search&keyword=${encodeURIComponent(keyword)}`);
-    if (limit == 1) return search.data[0]
-    else return search.data
-}
-async function getMetingLrcById(id) {
-    let meting = window.localStorage["lrcMetingUrl"],
-        server = 'netease',
-        lyricRegex = /\[([0-9.:]*)\]/i
-    result = (await axios.get(`${meting}?server=${server}&type=lrc&id=${id}`)).data
-    try {
-        result = result.lyric && result.tlyric ? migrate(result.lyric, result.tlyric) : result.lyric
-    } catch (e) {
-        result = result.lyric
-        console.error(e)
-    }
-    return result && result.match(lyricRegex) ? result : false
-}
