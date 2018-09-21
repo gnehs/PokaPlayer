ce93eb3ffee53a5b746bd79107c58ca6ed596dc3
gnehs <jayuiop@gmail.com>
2018-09-21 21:21:32 +0800

    修正歌詞捲動死掉掉

--- public/js/script.js
+++ public/js/script.js
@@ -825,7 +825,7 @@ async function showNow() {
                 $('[data-player] div[data-lrc="inner"] p').removeClass('mdui-text-color-theme-accent')
                 $('[data-player] div[data-lrc="inner"] p').eq(nowLrc).addClass('mdui-text-color-theme-accent')
                 let sh = $('div[data-lrc="inner"] p.mdui-text-color-theme-accent')[0].offsetTop - $('[data-player] .info>div[data-lrc]').height() / 2 - $('div[data-lrc="inner"] p.mdui-text-color-theme-accent')[0].clientHeight
-                $('[data-player] .info>div[data-lrc]').scrollTop({ scrollTop: sh }, 250);
+                $('[data-player] .info>div[data-lrc]').animate({ scrollTop: sh }, 250);
             }
         }
     });
