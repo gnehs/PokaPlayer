b1d5018665f3257eef54d91824e5604a296e0922
gnehs <jayuiop@gmail.com>
2018-10-26 22:32:08 +0800

    修改自動登入功能避免跳跳

--- public/js/login.js
+++ public/js/login.js
@@ -3,20 +3,23 @@ $(function() {
     var userPASS = window.localStorage["userPASS"]
     if (userPASS && userPASS != "false") {
         mdui.snackbar({
-            message: '偵測到已儲存的密碼，正在嘗試登入',
-            timeout: 1000
+            message: '偵測到已儲存的密碼',
+            buttonText: '登入',
+            onButtonClick: () => {
+                $.post("/login/", { userPASS: userPASS }, function(data) {
+                    if (data == 'success') {
+                        mdui.snackbar({ message: '登入成功', timeout: 1000 });
+                        document.location.href = "/";
+                    } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
+                });
+            },
+            timeout: 5000
         });
-        $.post("/login/", { userPASS: userPASS }, function(data) {
-            if (data == 'success') {
-                mdui.snackbar({ message: '登入成功', timeout: 1000 });
-                document.location.href = "/";
-            } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
-        });
+
     }
 });
 
 function check() {
-
     $.post("/login/", { userPASS: $("#userPASS").val() }, function(data) {
         if (data == 'success') {
             mdui.snackbar({ message: '登入成功', timeout: 1000 });
