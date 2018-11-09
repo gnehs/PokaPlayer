791fb8a24a08c338531afc974931a7fa94e3382e
gnehs <jayuiop@gmail.com>
2018-11-09 23:17:38 +0800

    登入頁面優化

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,44 @@
 // 初始化網頁
-$(function() {
-    var userPASS = window.localStorage["userPASS"]
+$(function () {
+    let userPASS = window.localStorage["userPASS"]
     if (userPASS && userPASS != "false") {
         mdui.snackbar({
             message: '偵測到已儲存的密碼',
             buttonText: '登入',
             onButtonClick: () => {
-                $.post("/login/", { userPASS: userPASS }, function(data) {
-                    if (data == 'success') {
-                        mdui.snackbar({ message: '登入成功', timeout: 1000 });
-                        document.location.href = "/";
-                    } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
-                });
+                login(userPASS)
             },
-            timeout: 5000
+            timeout: 10 * 1000 //10s
         });
-
     }
+    $("#userPASS").keypress(function (event) {
+        if (event.keyCode == 13) {
+            check()
+        }
+    });
 });
 
 function check() {
-    $.post("/login/", { userPASS: $("#userPASS").val() }, function(data) {
-        if (data == 'success') {
-            mdui.snackbar({ message: '登入成功', timeout: 1000 });
-            window.localStorage["userPASS"] = $("#userPASS").val()
-            document.location.href = "/";
-        } else mdui.snackbar({ message: '登入失敗', timeout: 1000 });
+    let userpass = $("#userPASS").val()
+    userpass ? login($("#userPASS").val()) : mdui.snackbar({
+        message: '密碼不得為空',
+        timeout: 1000
     });
-    return false
+}
+
+function login(password) {
+    $.post("/login/", {
+        userPASS: password
+    }, data => {
+        if (data == 'success') {
+            mdui.snackbar({
+                message: '登入成功',
+                timeout: 1000
+            });
+            document.location.href = "/";
+        } else mdui.snackbar({
+            message: '登入失敗',
+            timeout: 1000
+        });
+    });
 }
