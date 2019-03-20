1c46bc33f29ef4a0116bf64ddee6c4888d578f14
gnehs <jayuiop@gmail.com>
2019-03-20 20:31:36 +0800

    更新 pokaFE

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) { //註冊一個假的 sw
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('serviceWorker registered');
+        }).catch(registrationError => {
+            console.log('serviceWorker registration failed: ', registrationError);
+        });
+    });
+}
