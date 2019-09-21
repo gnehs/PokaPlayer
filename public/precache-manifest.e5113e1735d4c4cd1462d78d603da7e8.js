1f07efa74744248cd56bfc56989297e9c60753e7
gnehs <jayuiop@gmail.com>
2019-09-21 17:38:51 +0800

    修正進度條消失

---
+++ public/precache-manifest.e5113e1735d4c4cd1462d78d603da7e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9ab0e4aa80062a16486",
+    "url": "/css/app.6442e1a7.css"
+  },
+  {
+    "revision": "d9ab0e4aa80062a16486",
+    "url": "/js/app.fc1554f7.js"
+  },
+  {
+    "revision": "82df6f292be501db8ff0",
+    "url": "/css/chunk-vendors.33fbaed9.css"
+  },
+  {
+    "revision": "82df6f292be501db8ff0",
+    "url": "/js/chunk-vendors.cfe56c98.js"
+  },
+  {
+    "revision": "9dda358c1e1ac246ce615c3b64e0f678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1292b57cedad9cca119d3326c01ac2",
+    "url": "/img/dailyRecommendPlaylists.png"
+  },
+  {
+    "revision": "0dbaf805763d1e22f9c0a262a8e20903",
+    "url": "/img/dailyRecommendSongs.png"
+  },
+  {
+    "revision": "2b6acf0d036c6749ead7731790621868",
+    "url": "/img/hqPlaylist.png"
+  },
+  {
+    "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
+    "url": "/img/topPlaylist.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
+    "url": "/img/playlist.psd"
+  }
+];
