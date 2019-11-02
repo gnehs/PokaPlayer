2ff5071b7018dffb90eaa8a1f3cc3f249eabe9e4
gnehs <jayuiop@gmail.com>
2019-11-02 13:10:45 +0800

    release 1.1.2

--- public/precache-manifest.fe474cc35ba870f76776be941ef84dac.js
+++ public/precache-manifest.fe474cc35ba870f76776be941ef84dac.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "754c8d7a29047a9fcb72",
-    "url": "/css/app.37211c4d.css"
+    "revision": "5670f96a892526dc6205",
+    "url": "/css/app.58a07e0c.css"
   },
   {
-    "revision": "754c8d7a29047a9fcb72",
-    "url": "/js/app.37da963c.js"
+    "revision": "5670f96a892526dc6205",
+    "url": "/js/app.1a7c9fee.js"
   },
   {
     "revision": "3f5ce505da92637d590f",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.0c3b0bce.js"
   },
   {
-    "revision": "b75a811b1f353655a322e11b0bf1d6a0",
+    "revision": "a789f9d0d46f8b260a5fdfae9ee67393",
     "url": "/index.html"
   },
   {
@@ -24,23 +24,27 @@ self.__precacheManifest = [
     "url": "/img/dailyRecommendSongs.png"
   },
   {
-    "revision": "2b6acf0d036c6749ead7731790621868",
-    "url": "/img/hqPlaylist.png"
-  },
-  {
     "revision": "5f1292b57cedad9cca119d3326c01ac2",
     "url": "/img/dailyRecommendPlaylists.png"
+  },
+  {
+    "revision": "2b6acf0d036c6749ead7731790621868",
+    "url": "/img/hqPlaylist.png"
   },
   {
     "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
     "url": "/img/topPlaylist.png"
   },
   {
+    "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
+    "url": "/img/playlist.psd"
+  },
+  {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
-    "url": "/img/playlist.psd"
+    "revision": "1a671b7e841911e90430555ae7fe1b08",
+    "url": "/img/update.svg"
   }
 ];
