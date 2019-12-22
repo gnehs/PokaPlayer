1eb6693b8a6c78c270bd68d73d84ecba8a0d1f61
gnehs <jayuiop@gmail.com>
2019-12-22 16:25:15 +0800

    release 2.0.7

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.292986d11243a5925075d835ed2f5b83.js"
+  "/precache-manifest.5b40af7e8d896d12b4878aabae80ff54.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pokaplayerfrontend"});
