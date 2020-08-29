09db24f93b6cf2508042376d20965023610e9839
gnehs <jayuiop@gmail.com>
2020-08-29 15:17:10 +0800

    新增 config 填寫教學

--- pokaplayer.js
+++ pokaplayer.js
@@ -14,6 +14,7 @@ if (config) {
     const app = express(); // Node.js Web 架構
     pokaLog.log('INSTALL', `未讀取到 config.json`)
     pokaLog.log('INSTALL', `請參考 config-simple.json 與 Wiki 來建立設定檔`)
+    pokaLog.log('INSTALL', `config.json 設定教學: https://git.io/PokaConfigChinese`)
     app.use(express.static("install"))
     const server = require("http").createServer(app)
     server.listen(3000, () => {
