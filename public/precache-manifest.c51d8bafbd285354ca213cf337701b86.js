403b75d631078cca3a8b9e400c80c63a4bc1392b
gnehs <jayuiop@gmail.com>
2021-03-21 14:18:12 +0800

    Release 2.16.0

--- public/precache-manifest.c51d8bafbd285354ca213cf337701b86.js
+++ public/precache-manifest.c51d8bafbd285354ca213cf337701b86.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "60c794d2b40bd8288a28",
-    "url": "/css/app.85d26230.css"
+    "revision": "48994d0e3e5c64e55d94",
+    "url": "/css/app.8459e15e.css"
   },
   {
-    "revision": "60c794d2b40bd8288a28",
-    "url": "/js/app.d744c7da.js"
+    "revision": "48994d0e3e5c64e55d94",
+    "url": "/js/app.09907798.js"
   },
   {
-    "revision": "f117dadbaf2f23d08d73",
-    "url": "/css/chunk-vendors.f1ad94f4.css"
+    "revision": "9f81592e7b649460b923",
+    "url": "/css/chunk-vendors.237b2bef.css"
   },
   {
-    "revision": "f117dadbaf2f23d08d73",
-    "url": "/js/chunk-vendors.db0333d5.js"
+    "revision": "9f81592e7b649460b923",
+    "url": "/js/chunk-vendors.9689edf8.js"
   },
   {
-    "revision": "9858dfbef0125ccd7771aba696829b17",
+    "revision": "1b04877b01c8ccd247b416e5354c179f",
     "url": "/index.html"
   },
   {
@@ -32,44 +32,44 @@ self.__precacheManifest = [
     "url": "/img/hqPlaylist.png"
   },
   {
-    "revision": "f6aebc3ee57feb127891f68d0b3f44f4",
-    "url": "/img/loginHeader.svg"
-  },
-  {
     "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
     "url": "/img/playlist.psd"
-  },
-  {
-    "revision": "06387036315d4a6830c5462562b15133",
-    "url": "/img/pwa/playlist.png"
-  },
-  {
-    "revision": "ef73b151cd8eed896d7392c4f34356ad",
-    "url": "/img/pwa/home.png"
   },
   {
     "revision": "eafa3c03c415d0b76b6a9d2294e84580",
     "url": "/img/pwa/album.png"
   },
   {
-    "revision": "14127602e1dbdd8a42485579450c1801",
-    "url": "/img/pwa/shuffle.png"
+    "revision": "ef73b151cd8eed896d7392c4f34356ad",
+    "url": "/img/pwa/home.png"
   },
   {
-    "revision": "4f581342e0dc57b34a176ebd1e491af0",
-    "url": "/img/splashscreens/ipad_splash.png"
+    "revision": "06387036315d4a6830c5462562b15133",
+    "url": "/img/pwa/playlist.png"
   },
   {
     "revision": "31f437d1172743a0330439681d0cf2b2",
     "url": "/img/pwa/pwa_shortcuts.psd"
   },
   {
-    "revision": "d330a0b6e9c5bb917eee03b016f3f9e3",
-    "url": "/img/splashscreens/ipadpro1_splash.png"
+    "revision": "14127602e1dbdd8a42485579450c1801",
+    "url": "/img/pwa/shuffle.png"
+  },
+  {
+    "revision": "f6aebc3ee57feb127891f68d0b3f44f4",
+    "url": "/img/loginHeader.svg"
   },
   {
     "revision": "35b6cf131562d21081a90a784f879b19",
     "url": "/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "4f581342e0dc57b34a176ebd1e491af0",
+    "url": "/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "d330a0b6e9c5bb917eee03b016f3f9e3",
+    "url": "/img/splashscreens/ipadpro1_splash.png"
   },
   {
     "revision": "f2d3ff48a22d9bb54508e0f520b6441d",
@@ -80,24 +80,24 @@ self.__precacheManifest = [
     "url": "/img/splashscreens/ipadpro3_splash.png"
   },
   {
+    "revision": "b5011363a9bbaf722e223d41fc23f865",
+    "url": "/img/splashscreens/iphonexr_splash.png"
+  },
+  {
     "revision": "6e7952c76307631bb228063e249d9c4a",
     "url": "/img/splashscreens/iphone6_splash.png"
   },
   {
-    "revision": "f8b2c6d67c3707610f3c2ad4285af48a",
-    "url": "/img/splashscreens/iphoneplus_splash.png"
+    "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
+    "url": "/img/topPlaylist.png"
   },
   {
     "revision": "ddface7d19acc6fa50ddea328d4a8037",
     "url": "/img/splashscreens/iphonex_splash.png"
   },
   {
-    "revision": "872c2998f0ea8d5f27cfd2916af1ffd4",
-    "url": "/img/splashscreens/iphonexsmax_splash.png"
-  },
-  {
-    "revision": "b5011363a9bbaf722e223d41fc23f865",
-    "url": "/img/splashscreens/iphonexr_splash.png"
+    "revision": "f8b2c6d67c3707610f3c2ad4285af48a",
+    "url": "/img/splashscreens/iphoneplus_splash.png"
   },
   {
     "revision": "f805f14e7d9ad7ee02c48cfeab58abb4",
@@ -108,8 +108,8 @@ self.__precacheManifest = [
     "url": "/README.md"
   },
   {
-    "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
-    "url": "/img/topPlaylist.png"
+    "revision": "872c2998f0ea8d5f27cfd2916af1ffd4",
+    "url": "/img/splashscreens/iphonexsmax_splash.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3490b19cc65dd573e7f85c9df46029d.js"
+  "/precache-manifest.c51d8bafbd285354ca213cf337701b86.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pokaplayerfrontend"});
