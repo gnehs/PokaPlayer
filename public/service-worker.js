551e39c3c05335f14f14c0aa645893531b0fc4e9
gnehs <jayuiop@gmail.com>
2021-03-24 16:20:15 +0800

    Release 2.16.2

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b796d90e174d3dec821d3070b7276c2.js"
+  "/precache-manifest.aa5cc9a55ae551046e1abfbd66674c55.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pokaplayerfrontend"});
