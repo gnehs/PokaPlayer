923e97f4830423532b82fbb478709f211d47f9da
gnehs <jayuiop@gmail.com>
2021-07-28 23:02:48 +0800

    added qqmusic lyric source

--- dataModule/netease2.js
+++ dataModule/netease2.js
@@ -1,8 +1,7 @@
 const jar = require("request").jar();
 const rp = require("request-promise")
-const request = require("request").defaults({
-    jar
-});
+const request = require("request").defaults({ jar });
+const { migrate, zhconvert } = require('./lyricUtils')
 const fs = require("fs-extra");
 const pokaLog = require("../log"); // 可愛控制台輸出
 const schedule = require("node-schedule"); // 很會計時ㄉ朋友 
@@ -97,156 +96,6 @@ const normalOptions = async (url, req = {}) => {
 
 const imageUrl = x => `/pokaapi/req/?moduleName=Netease2&data=${encodeURIComponent(genReq(x))}`;
 
-function migrate(org, t, offset = 10 ** -3) {
-    const isDigit = x => !isNaN(Number(x));
-
-    const plus = (num1, num2, ...others) => {
-        // 精確加法
-        if (others.length > 0) return plus(plus(num1, num2), others[0], ...others.slice(1));
-        const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
-        return (times(num1, baseNum) + times(num2, baseNum)) / baseNum;
-    };
-    const digitLength = num => {
-        // Get digit length of e
-        const eSplit = num.toString().split(/[eE]/);
-        const len = (eSplit[0].split(".")[1] || "").length - +(eSplit[1] || 0);
-        return len > 0 ? len : 0;
-    };
-    const times = (num1, num2, ...others) => {
-        // 精確乘法
-        function checkBoundary(num) {
-            if (num > Number.MAX_SAFE_INTEGER || num < Number.MIN_SAFE_INTEGER) {
-                console.warn(`${num} is beyond boundary when transfer to integer, the results may not be accurate`);
-            }
-        }
-
-        function float2Fixed(num) {
-            if (num.toString().indexOf("e") === -1) return Number(num.toString().replace(".", ""));
-            const dLen = digitLength(num);
-            return dLen > 0 ? num * Math.pow(10, dLen) : num;
-        }
-
-        if (others.length > 0) return times(times(num1, num2), others[0], ...others.slice(1));
-        const num1Changed = float2Fixed(num1);
-        const num2Changed = float2Fixed(num2);
-        const baseNum = digitLength(num1) + digitLength(num2);
-        const leftValue = num1Changed * num2Changed;
-
-        checkBoundary(leftValue);
-
-        return leftValue / Math.pow(10, baseNum);
-    };
-    const minus = (num1, num2, ...others) => {
-        // 精確減法
-        if (others.length > 0) return minus(minus(num1, num2), others[0], ...others.slice(1));
-        const baseNum = Math.pow(10, Math.max(digitLength(num1), digitLength(num2)));
-        return (times(num1, baseNum) - times(num2, baseNum)) / baseNum;
-    };
-    const strip = (x, precision = 12) => +parseFloat(x.toPrecision(precision)); // 數字精確化
-
-    const tagToTime = tag =>
-        isDigit(tag[0]) ?
-            tag
-                .split(":")
-                .reverse()
-                .reduce((acc, cur, index) => plus(acc, Number(cur) * 60 ** index), 0) :
-            tag;
-    const parse = (x, isTranslated = false) => {
-        let pLyricLines = x
-            .split("\n")
-            .filter(x => x != "")
-            .map(str => {
-                const regex = /\[(\d+:\d+\.\d+)\]/gm;
-                let m;
-
-                let result = [];
-
-                while ((m = regex.exec(str)) !== null) {
-                    if (m.index === regex.lastIndex) regex.lastIndex++;
-                    result.push(m[1]);
-                }
-                result.push(str.match(/.+\]((?:.|^$)*)/)[1]);
-                return result;
-            });
-        let result = [];
-        for (let pLyricLine of pLyricLines) {
-            let lyric = pLyricLine.pop();
-            for (let time of pLyricLine) {
-                result.push([tagToTime(time), lyric, isTranslated]);
-            }
-        }
-        return result;
-    };
-
-    const timeToTag = seconds => {
-        let minute = Math.floor(seconds / 60);
-        let second = minus(seconds, minute * 60);
-        return `${minute}:${second}`;
-    };
-
-    // 開始切成 [(tag, lyric)]
-
-    parsedLyrics = parse(org)
-        .concat(parse(t, true))
-        .sort((a, b) => {
-            if ((typeof a[0] == typeof b[0]) == "string") return 0;
-            else if (typeof a[0] == "string") return -1;
-            else if (typeof b[0] == "string") return 1;
-            else {
-                if (a[0] == b[0]) return a[2] ? 1 : -1;
-                else return a[0] < b[0] ? -1 : 1;
-            }
-        });
-
-    // 整理成 [[time, [orgLyric, tLyric]]]
-    let parsedLyricPairs = [];
-
-    let i = 0;
-    while (i < parsedLyrics.length) {
-        if (typeof parsedLyrics[i][0] == "string") {
-            parsedLyricPairs.push(parsedLyrics[i]);
-            i += 1;
-        } else if (i != parsedLyrics.length - 1) {
-            if (parsedLyrics[i][0] == parsedLyrics[i + 1][0]) {
-                parsedLyricPairs.push([parsedLyrics[i][0],
-                [parsedLyrics[i][1], parsedLyrics[i + 1][1]]
-                ]);
-                i += 2;
-            } else {
-                parsedLyricPairs.push([parsedLyrics[i][0],
-                [parsedLyrics[i][1], parsedLyrics[i][1]]
-                ]);
-                i += 1;
-            }
-        } else {
-            parsedLyricPairs.push([parsedLyrics[i][0],
-            [parsedLyrics[i][1], parsedLyrics[i][1]]
-            ]);
-            i += 1;
-        }
-    }
-
-    // 壓回 LRC
-    let result = "";
-    for (let i in parsedLyricPairs) {
-        i = Number(i);
-        if (typeof parsedLyricPairs[i][0] == "string") result += `[${parsedLyricPairs[i][0]}]\n`;
-        else {
-            if (i != parsedLyricPairs.length - 1) {
-                result += `[${timeToTag(parsedLyricPairs[i][0])}]${parsedLyricPairs[i][1][0]}\n[${timeToTag(
-                    plus(parsedLyricPairs[i + 1][0], -offset)
-                )}]${parsedLyricPairs[i][1][1]}\n`;
-            } else {
-                result += `[${timeToTag(parsedLyricPairs[i][0])}]${parsedLyricPairs[i][1][0]}\n[${timeToTag(
-                    parsedLyricPairs[i][0]
-                )}]${parsedLyricPairs[i][1][1]}\n`;
-            }
-        }
-    }
-
-    return result;
-}
-
 async function login(config) {
     let result;
     if (config.login.phone) {
@@ -256,23 +105,14 @@ async function login(config) {
     } else {
         result = await rp(options(`${server}login?email=${config.login.email}&password=${config.login.password}`));
     }
+    if (!result) {
+        pokaLog.logDMErr('Netease2', `登入失敗`)
+        return
+    }
     isLoggedin = result.code == 200;
     userId = result.profile.userId
     pokaLog.logDM('Netease2', `${result.profile.nickname}(${userId}) 登入${isLoggedin ? "成功" : "失敗"}`)
     return result;
-}
-// 繁化姬
-async function chsToCht(text, converter = "Taiwan") {
-    let result = await rp({
-        method: "POST",
-        uri: "https://api.zhconvert.org/convert",
-        body: {
-            converter,
-            text
-        },
-        json: true
-    });
-    return result.data.text;
 }
 //自動重新登入
 schedule.scheduleJob("0 0 * * *", async function () {
@@ -663,7 +503,7 @@ async function getPlaylists(uid) {
                     pokaLog.logDMErr('Netease2', `${playlistId} 的分類出錯，已修正為預設`)
                     config[playlistId].category = ["ACG", "日语", "欧美"];
                 }
-                let translatedCategory = await chsToCht(category)
+                let translatedCategory = await zhconvert(category)
                 r.push({
                     name: `${translatedCategory} - ${playlistId == 'hqPlaylist' ? '精品' : '精選'}歌單`,
                     source: "Netease2",
@@ -823,14 +663,14 @@ async function getLyric(id) {
         if (result.nolyric) lyric = "[0:0] 純音樂";
         else if (result.tlyric && result.tlyric.lyric) { //翻譯後的歌詞
             try {
-                lyric = migrate(result.lrc.lyric, await chsToCht(result.tlyric.lyric));
+                lyric = migrate(result.lrc.lyric, await zhconvert(result.tlyric.lyric));
             } catch (e) {
                 pokaLog.logDMErr('Netease2', `歌詞繁化錯誤 ${e.toString()}`)
                 lyric = result.lrc.lyric;
             }
         } else if (result.lrc && result.lrc.lyric) { // 中文歌詞
             try {
-                lyric = await chsToCht(result.lrc.lyric, "Traditional");
+                lyric = await zhconvert(result.lrc.lyric, "Traditional");
             } catch (e) {
                 pokaLog.logDMErr('Netease2', `歌詞繁化錯誤 ${e.toString()}`)
                 lyric = result.lrc.lyric;
---
+++ dataModule/qqmusic.js
@@ -0,0 +1,84 @@
+const fetch = require('node-fetch')
+const { migrate, zhconvert } = require('./lyricUtils')
+const pokaLog = require("../log"); // 可愛控制台輸出
+const lyricTimeout = new Promise(function (resolve, reject) {
+    setTimeout(resolve, 5000, null);
+});
+async function searchLyrics(keyword) {
+    let searchResult = await fetch(`https://c.y.qq.com/soso/fcgi-bin/client_search_cp?w=${encodeURI(keyword)}&format=json&cr=1&g_tk=5381`, {
+        method: "GET",
+        headers: {
+            "Referer": "y.qq.com/portal/player.html",
+            "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:46.0) Gecko/20100101 Firefox/46.0'
+        }
+    })
+        .then(res => res.json())
+        .then(x => x.data.song.list)
+        .then(x => x.slice(0, 5))
+    searchResult = searchResult.map(async y => {
+        if (!y.songmid || !y.songmid) return null
+        let lyric = await getLyric(y.songmid)
+        if (!lyric) {
+            // console.log(`${y.songname} timeout.`)
+            return null
+        }
+        return {
+            name: y.songname,
+            artist: y.singer.map(x => x.name).join(`、`),
+            source: "QQMusic",
+            id: y.songmid,
+            lyric
+        }
+    })
+
+    return { lyrics: (await Promise.all(searchResult)).filter(x => x) };
+}
+function getLyric(id) {
+    return fetch(`https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg?songmid=${encodeURI(id)}&g_tk=5381&format=json`, {
+        method: "GET",
+        headers: {
+            Referer: 'https://y.qq.com',
+        }
+    })
+        .then(res => res.json())
+        .then(async x => {
+            if (x.lyric) {
+                let lyric, tlyric, result
+                lyric = Buffer.from(x.lyric, 'base64').toString()
+                try {
+                    tlyric = Buffer.from(x.trans, 'base64').toString()
+                } catch (e) { }
+                if (tlyric) {
+                    tlyric = await zhconvert(tlyric)
+                    result = migrate(lyric, tlyric)
+                } else {
+                    lyric = await zhconvert(lyric, "Traditional")
+                    result = lyric
+                }
+                return result
+            }
+        })
+}
+async function onLoaded() {
+    try {
+        let res = await searchLyrics(`世界で一番恋してる 喜多修平`)
+        if (res.lyrics.length) {
+            pokaLog.logDM('QQMusic', `Lyric 測試正常`)
+            pokaLog.logDM('QQMusic', new Date().toLocaleString())
+            console.log(res.lyrics.length)
+            return res.lyrics.length
+        } else {
+            return 0
+        }
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}
+
+module.exports = {
+    name: "QQMusic",
+    enabled: true,
+    onLoaded,
+    searchLyrics,
+};
