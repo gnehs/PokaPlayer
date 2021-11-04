00ed43c3a53973ecc767c65576b9045d02993bb0
gnehs <jayuiop@gmail.com>
2021-11-04 21:46:10 +0800

    release 3.4.13

--- db/session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const model = mongoose.model('Session', new mongoose.Schema({}))
-async function clearAll() {
-    return await model.deleteMany({})
-}
-module.exports = {
-    model,
-    clearAll
-}
--- index.js
+++ index.js
@@ -95,28 +95,6 @@ app
         }
         res.json(u)
     })
-    .post("/clear-session/", async (req, res) => {
-        let { username, password } = req.body
-        let u = await User.login({ username, password })
-        if (u.success && await User.isUserAdmin(u.user)) {
-            try {
-                await require('./db/session').clearAll()
-            } catch (e) {
-                return res.json({ success: false, e })
-            }
-            res.json({ success: true })
-
-            addLog({
-                level: "warn",
-                type: "system",
-                event: "Session cleared.",
-                user: req.session.user,
-                description: `User {${req.session.user}} clearned session`
-            })
-        } else {
-            res.json({ success: false, e: 'Permission Denied Desu' })
-        }
-    })
     .get("/logout/", (req, res) => {
         // 登出
         if (req.session.user) {
