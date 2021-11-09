e15eb8e19c3a9db1496fff5bc1a904032f0b3263
gnehs <jayuiop@gmail.com>
2021-11-09 22:23:08 +0800

    var -> let

--- dataModule.js
+++ dataModule.js
@@ -467,15 +467,15 @@ router.get("/searchLyrics/", async (req, res) => {
             s1 = s1.toLowerCase();
             s2 = s2.toLowerCase();
 
-            var costs = new Array();
+            let costs = new Array();
             for (var i = 0; i <= s1.length; i++) {
-                var lastValue = i;
-                for (var j = 0; j <= s2.length; j++) {
+                let lastValue = i;
+                for (let j = 0; j <= s2.length; j++) {
                     if (i == 0)
                         costs[j] = j;
                     else {
                         if (j > 0) {
-                            var newValue = costs[j - 1];
+                            let newValue = costs[j - 1];
                             if (s1.charAt(i - 1) != s2.charAt(j - 1))
                                 newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
                             costs[j - 1] = lastValue;
@@ -488,13 +488,13 @@ router.get("/searchLyrics/", async (req, res) => {
             }
             return costs[s2.length];
         }
-        var longer = s1;
-        var shorter = s2;
+        let longer = s1;
+        let shorter = s2;
         if (s1.length < s2.length) {
             longer = s2;
             shorter = s1;
         }
-        var longerLength = longer.length;
+        let longerLength = longer.length;
         if (longerLength == 0) {
             return 1.0;
         }
