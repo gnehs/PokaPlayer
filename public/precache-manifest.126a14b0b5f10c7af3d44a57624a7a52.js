2b9e89f39a62ab48247349a4232398730525c1ec
gnehs <jayuiop@gmail.com>
2021-12-31 15:56:21 +0800

    updated frontend

--- public/precache-manifest.126a14b0b5f10c7af3d44a57624a7a52.js
+++ public/precache-manifest.126a14b0b5f10c7af3d44a57624a7a52.js
@@ -1,59 +1,31 @@
-self.__precacheManifest = [
+self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "903fc111a46cb4c8a648",
-    "url": "/css/app.419244e1.css"
+    "revision": "f10254a806d67417a6d0fb0062bb2b94",
+    "url": "/README.md"
   },
   {
-    "revision": "903fc111a46cb4c8a648",
-    "url": "/js/app.de18db80.js"
+    "revision": "5b66836dd67f47c758df",
+    "url": "/css/app.44ed9f47.css"
   },
   {
-    "revision": "354dfbe294ef4030426e",
-    "url": "/css/chunk-b8a804c8.2f781aa3.css"
+    "revision": "8f2ae97393e5e66eb4c0",
+    "url": "/css/chunk-b8a804c8.c67975b2.css"
   },
   {
-    "revision": "354dfbe294ef4030426e",
-    "url": "/js/chunk-b8a804c8.e498c5bd.js"
+    "revision": "515dfc7e124e2e71ecb6",
+    "url": "/css/chunk-c84dfbd6.1fac8e93.css"
   },
   {
-    "revision": "086bdf164eda117321eb",
-    "url": "/css/chunk-dd345c24.150e9938.css"
+    "revision": "e81aa7e48612474c8a44",
+    "url": "/css/chunk-vendors.ac13731e.css"
   },
   {
-    "revision": "086bdf164eda117321eb",
-    "url": "/js/chunk-dd345c24.ad0a4c0d.js"
+    "revision": "0a7b25aeb91d27c7f7d5",
+    "url": "/css/setting-admin.1a0ae98c.css"
   },
   {
-    "revision": "bbdabf18486785a3b15b",
-    "url": "/css/chunk-vendors.7ed6c5a4.css"
-  },
-  {
-    "revision": "bbdabf18486785a3b15b",
-    "url": "/js/chunk-vendors.d5d978d4.js"
-  },
-  {
-    "revision": "b19ec9c2128144eef247",
+    "revision": "44a1229a4f8565199a10",
     "url": "/css/setting.aae77af4.css"
-  },
-  {
-    "revision": "b19ec9c2128144eef247",
-    "url": "/js/setting.60f989d3.js"
-  },
-  {
-    "revision": "7e04c8446c4e66828dce",
-    "url": "/css/setting-admin.f59baf5b.css"
-  },
-  {
-    "revision": "7e04c8446c4e66828dce",
-    "url": "/js/setting-admin.a7599abc.js"
-  },
-  {
-    "revision": "abda8fb6e651e3afbd857c20f634dd5a",
-    "url": "/index.html"
-  },
-  {
-    "revision": "2b6acf0d036c6749ead7731790621868",
-    "url": "/img/hqPlaylist.png"
   },
   {
     "revision": "5f1292b57cedad9cca119d3326c01ac2",
@@ -64,8 +36,16 @@ self.__precacheManifest = [
     "url": "/img/dailyRecommendSongs.png"
   },
   {
+    "revision": "2b6acf0d036c6749ead7731790621868",
+    "url": "/img/hqPlaylist.png"
+  },
+  {
     "revision": "f6aebc3ee57feb127891f68d0b3f44f4",
     "url": "/img/loginHeader.svg"
+  },
+  {
+    "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
+    "url": "/img/playlist.psd"
   },
   {
     "revision": "eafa3c03c415d0b76b6a9d2294e84580",
@@ -76,20 +56,16 @@ self.__precacheManifest = [
     "url": "/img/pwa/home.png"
   },
   {
-    "revision": "aab8fae5732e190d7e0b2b52bb77e7fc",
-    "url": "/img/playlist.psd"
-  },
-  {
-    "revision": "14127602e1dbdd8a42485579450c1801",
-    "url": "/img/pwa/shuffle.png"
-  },
-  {
     "revision": "06387036315d4a6830c5462562b15133",
     "url": "/img/pwa/playlist.png"
   },
   {
     "revision": "31f437d1172743a0330439681d0cf2b2",
     "url": "/img/pwa/pwa_shortcuts.psd"
+  },
+  {
+    "revision": "14127602e1dbdd8a42485579450c1801",
+    "url": "/img/pwa/shuffle.png"
   },
   {
     "revision": "e0c0ca7d2e1b3d32233ab4783fc73106",
@@ -104,20 +80,20 @@ self.__precacheManifest = [
     "url": "/img/splashscreens/ipadpro1_splash.png"
   },
   {
-    "revision": "38e64c59bdd2bb89675eb334324cc8b9",
-    "url": "/img/splashscreens/ipadpro3_splash.png"
-  },
-  {
     "revision": "35b6cf131562d21081a90a784f879b19",
     "url": "/img/splashscreens/ipadpro2_splash.png"
   },
   {
-    "revision": "6e7952c76307631bb228063e249d9c4a",
-    "url": "/img/splashscreens/iphone6_splash.png"
+    "revision": "38e64c59bdd2bb89675eb334324cc8b9",
+    "url": "/img/splashscreens/ipadpro3_splash.png"
   },
   {
     "revision": "f2d3ff48a22d9bb54508e0f520b6441d",
     "url": "/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "6e7952c76307631bb228063e249d9c4a",
+    "url": "/img/splashscreens/iphone6_splash.png"
   },
   {
     "revision": "f8b2c6d67c3707610f3c2ad4285af48a",
@@ -136,19 +112,47 @@ self.__precacheManifest = [
     "url": "/img/splashscreens/iphonexsmax_splash.png"
   },
   {
-    "revision": "f10254a806d67417a6d0fb0062bb2b94",
-    "url": "/README.md"
+    "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
+    "url": "/img/topPlaylist.png"
   },
   {
     "revision": "f805f14e7d9ad7ee02c48cfeab58abb4",
     "url": "/img/update.svg"
   },
   {
-    "revision": "d50b11f2b0b92b6fa9bbf1700ea567bf",
-    "url": "/img/topPlaylist.png"
+    "revision": "7f83ea50db207702d6dbbe0ce2c31b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b66836dd67f47c758df",
+    "url": "/js/app.235cb745.js"
+  },
+  {
+    "revision": "8f2ae97393e5e66eb4c0",
+    "url": "/js/chunk-b8a804c8.89cb72da.js"
+  },
+  {
+    "revision": "515dfc7e124e2e71ecb6",
+    "url": "/js/chunk-c84dfbd6.03ed44cc.js"
+  },
+  {
+    "revision": "e81aa7e48612474c8a44",
+    "url": "/js/chunk-vendors.b81738e3.js"
+  },
+  {
+    "revision": "0a7b25aeb91d27c7f7d5",
+    "url": "/js/setting-admin.148fc279.js"
+  },
+  {
+    "revision": "44a1229a4f8565199a10",
+    "url": "/js/setting.89ba27e5.js"
+  },
+  {
+    "revision": "9736d795adb7f25423560e61fc4021b5",
+    "url": "/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   }
-];
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7bc41e4019cb4373458dbfa3063b9c86.js"
+  "/precache-manifest.126a14b0b5f10c7af3d44a57624a7a52.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pokaplayerfrontend"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "pokaplayerfrontend"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
