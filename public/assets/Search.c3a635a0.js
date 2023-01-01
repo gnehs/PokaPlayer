bc6ee180da3148d8bd2d534948c8adaaa543272b
gnehs <jayuiop@gmail.com>
2023-01-01 23:27:21 +0800

    updated frontend

--- public/assets/Search.c3a635a0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index.3eb2391f.js";const n={};function o(t,a){return c(),r("h1",null,"Search")}const _=e(n,[["render",o]]);export{_ as default};
--- public/assets/empty.c1b36d36.js
+++
@@ -1,1 +0,0 @@
-import{o as e,g as r,m as t,R as a}from"./index.3eb2391f.js";const n={__name:"empty",setup(o){return(s,c)=>(e(),r(t(a)))}};export{n as default};
