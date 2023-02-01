b1a4434454cccf21028a5742fba792cd6d00dfe8
gnehs <jayuiop@gmail.com>
2023-02-01 14:17:29 +0800

    release 4.0.8

--- public/assets/Log-c4f1fe24.js
+++
@@ -1,6 +0,0 @@
-import{_ as h,j as B,i as o,J as x,k as I,r as P,o as i,c as b,e as f,b as m,t as y,T as C,s as E,F as j}from"./index-e1e3d4ec.js";const A={__name:"Log",setup(D){const _=B("PokaAPI"),t=o(""),L=o(0),g=o(!1),a=o([]);async function p(){let e=await _.getUserList();a.value=e}async function v(e=0){var s;(!a.value||!a.value.length)&&await p();let n=await _.getLog(e);for(let{level:k,type:w,event:$,user:l,description:c,time:u}of n)l=((s=a.value.find(r=>r._id==l))==null?void 0:s.username)||l,u=new Date(u).toLocaleString(),a.value.map(r=>{c=c.replace(new RegExp(`{${r._id}}`,"g"),r.username)}),t.value+=`[${k}] ${w} / ${$}
-`,t.value+=`  📄 ${c}
-`,t.value+=`  👤 ${l}
-`,t.value+=`  🕒 ${u}
-`,t.value+=`
-`;n.length||(g.value=!0)}const d=o(null);return x(d,async([{isIntersecting:e}],n)=>{e&&await v(L.value++)},{threshold:.5,rootMargin:"0px"}),I(async()=>{await p(),await v()}),(e,n)=>{const s=P("Loader");return i(),b(j,null,[(i(),f(C,{to:"#header-center"},[m("p",null,y(e.$t("settings.log.title")),1)])),m("pre",{class:"log",ref:"logContainer"},y(t.value),513),g.value?E("",!0):(i(),f(s,{key:0,ref_key:"logBottom",ref:d},null,512))],64)}}},M=h(A,[["__scopeId","data-v-640354d8"]]);export{M as default};
---
+++ public/assets/user-8fc11de1.js
@@ -0,0 +1,1 @@
+import{h as o,i as t}from"./index-55e28da3.js";const u=o("user",()=>{const e=t({});function r(s){e.value=s}return{userInfo:e,setUserInfo:r}});export{u};
